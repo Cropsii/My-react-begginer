ef976425be3acb54b5292025d35af6a01b76d0b1
Cropsi <soap.duskyla.81@mail.ru>
2024-09-16 19:24:12 +0300

    New

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function plus() {
+    setCount(count + 1);
+  }
+  function minus() {
+    if (count === 0) {
+      setCount = 0;
+    }
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={minus} className="minus">
+          - Минус
+        </button>
+        <button onClick={plus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
