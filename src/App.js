7933c746e744cadc43ef070ccaf37e206724258c
Cropsi <soap.duskyla.81@mail.ru>
2024-12-02 20:39:33 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,112 @@
-import './index.scss';
+import React, { useState } from "react";
+import "./index.scss";
+import { clear } from "@testing-library/user-event/dist/clear";
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: "Что такое JSX?",
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
     ],
     correct: 2,
   },
 ];
-
-function Result() {
+function Result({ Back, cor, Clear }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>{`Вы отгадали ${cor} ответа из ${questions.length}`}</h2>
+      <button
+        onClick={() => {
+          Back();
+          Clear();
+        }}
+      >
+        Попробовать снова
+      </button>
     </div>
   );
 }
 
-function Game() {
+function Game({ question, HandleStep, step, questions, corAdd, cor }) {
+  function ProsCount(step, questions) {
+    return (step / questions.length) * 100;
+  }
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div
+          style={{ width: `${ProsCount(step, questions)}%` }}
+          className="progress__inner"
+        ></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, id) => (
+          <li
+            key={id}
+            onClick={() => {
+              HandleStep(step);
+              corAdd(id, question.correct, cor);
+            }}
+          >
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [cor, setCor] = useState(0);
+  const [step, setStep] = useState(0);
+  const question = questions[step];
+  function corAdd(index, correct, cor) {
+    if (index == correct) {
+      setCor(cor + 1);
+    }
+  }
+  function HandleStep(step) {
+    return setStep(step + 1);
+  }
+  function Back() {
+    return setStep(0);
+  }
+  function Clear() {
+    return setCor(0);
+  }
+  console.log(question);
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step == questions.length ? (
+        <Result Back={Back} cor={cor} Clear={Clear}></Result>
+      ) : (
+        <Game
+          cor={cor}
+          corAdd={corAdd}
+          question={question}
+          step={step}
+          HandleStep={HandleStep}
+          questions={questions}
+        />
+      )}
     </div>
   );
 }
